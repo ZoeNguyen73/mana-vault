f63ff8d7bf63de64af6c3bfd8a0054479ff6e892
Zoe Nguyen <nganguyen0703@gmail.com>
2025-07-25 15:08:52 +0800

    feat: add axios api

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
+import SearchBar from "../components/SearchBar";
+
 const Home = () => {
   console.log("Home page rendered");
   return (
     <div className="text-4xl text-text tracking-wide border border-red-500">
       Home page
+      <SearchBar />
     </div>
   )
 }
